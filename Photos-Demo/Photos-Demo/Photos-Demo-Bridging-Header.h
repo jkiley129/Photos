1e262ad31d31be97acecf000a752facf453f44b1
jkiley129 <josephkiley@icloud.com>
2016-06-06 22:24:32 -0400

    adding in bridging header

---
+++ Photos-Demo/Photos-Demo/Photos-Demo-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Photos-Demo-Bridging-Header.h
+//  Photos-Demo
+//
+//  Created by Joseph Kiley on 6/6/16.
+//  Copyright © 2016 Joseph Kiley. All rights reserved.
+//
+
+#ifndef Photos_Demo_Bridging_Header_h
+#define Photos_Demo_Bridging_Header_h
+
+#import "Haneke.h"
+
+
+#endif /* Photos_Demo_Bridging_Header_h */
